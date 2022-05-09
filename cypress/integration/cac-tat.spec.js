20108c836f25fcc2ed8f825ea55fb11e8490cec9
ElcimarSilva <elcimar8@gmail.com>
2022-05-09 16:42:48 -0300

    altercoes

--- cypress/integration/cac-tat.spec.js
+++ cypress/integration/cac-tat.spec.js
@@ -87,7 +87,36 @@ describe('Central de atendimento ao cliente tat', function() {
         cy.get(".success").should('not.be.visible')
     })
 
-    it('', function(){
+    it('Seleciona um produto (YouTube) por seu texto', function(){
+        cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
+    })
 
+    it('Seleciona um produto (Mentoria) por seu valor (value)', function(){
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
+    })
+
+    it('Seleciona um produto (Blog) por seu índice', function(){
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+
+    it('Marca o tipo de atendimento "Feedback"', function(){
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+
+    it.only('Marca cada tipo de atendimento', function(){
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio) {
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
     })
 })
